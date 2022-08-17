2898487cfefbd68e638c87efbc0bc905c4967b00
hahacandy <58638224+hahacandy@users.noreply.github.com>
2022-08-18 03:57:51 +0900

    Error fix when anime save3

--- funimation_downloader.py
+++ funimation_downloader.py
@@ -113,13 +113,12 @@ def download_funimation_anime(driver, anime_url, save_path):
 
                 if vtt_url != None and m3u8_url != None:
 
-                    vtt_name = str(idx) + '_' + anime_episode['name'] + '.vtt'
-                    video_name = str(idx) + '_' + anime_episode['name'] + '.mp4'
-                    
                     #ファイルをセーブするときエラーを避けるため
                     anime_name = re.sub(r'[\\/:*?"<>|]', '', save_path2)
-                    vtt_name = re.sub(r'[\\/:*?"<>|]', '', vtt_name)
-                    video_name = re.sub(r'[\\/:*?"<>|]', '', video_name)
+                    anime_episode_name = re.sub(r'[\\/:*?"<>|]', '', anime_episode['name'])
+                    
+                    vtt_name = str(idx) + '_' + anime_episode_name + '.vtt'
+                    video_name = str(idx) + '_' + anime_episode_name + '.mp4'
                     
                     save_path2 = save_path + anime_name + '/'
 
